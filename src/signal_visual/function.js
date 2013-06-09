d7eb5b5437344d0772f4b7a93f502d277bf1b927
bxy-win8 <bxy@mails.tsinghua.edu.cn>
2013-06-09 14:52:00 +0800

    signal-visual improvement!

--- src/signal_visual/function.js
+++ src/signal_visual/function.js
@@ -6,29 +6,54 @@ var log_draw=document.getElementById("log_draw");
 var sar_draw=document.getElementById("sar_draw");
 var log_context = log_draw.getContext("2d");
 var sar_context = sar_draw.getContext("2d");
-var canvas_size={width:1000,height:200};
+var canvas_size={width:1000,height:250};
+var border_left = 50;
+var border_bottom = 20;
+var inside_width = 900;
+var inside_height = 200;
+function position_inside(x,y) {
+	x = border_left + x*(inside_width);
+	y = canvas_size.height - border_bottom - y*(inside_height);
+	console.warn(x,y);
+	return {'x':x,'y':y};
+}
+function draw_base_line(max,min) {
+	
+}
 function draw_sar(){
 	sar_draw.width = canvas_size.width;
-	sar_context.fillStyle = "rgba(0,189,0,0.8)";
-  sar_context.fillRect(0,0,300,300);
+	sar_draw.height = canvas_size.height;
+	
   findAll('Sar_signal', nodes[current_node_index], 
   	{'logTime':{'$gte':time.start_time+current_day_index*seconds_in_day,
   							'$lte':time.start_time+(current_day_index+1)*seconds_in_day},
   	 'tempID':sars[current_sar_index].pos}, 
   	{'logTime':1},{'logTime':1},function(vector){
   		for(var i = 1; i < vector.length; i++) {
-  			var square_width = 10*60/seconds_in_day*canvas_size.width;
-  			console.warn(vector[i]);
-  			var x = (vector[i]['logTime'] - time.start_time - current_day_index*seconds_in_day)/seconds_in_day*canvas_size.width;
-  			sar_context.fillRect(x-square_width/2,0,square_width,300);
+		  		var square_width = 10*60/seconds_in_day;
+	  			var x = (vector[i]['logTime'] - time.start_time - current_day_index*seconds_in_day)/seconds_in_day;
+					sar_context.fillStyle = "rgba(0,189,0,0.5)";
+	  			var inside_pos_1 = position_inside(x-square_width/2,1);
+	  			var inside_pos_2 = position_inside(x+square_width/2,0);
+	  			sar_context.fillRect(inside_pos_1.x,inside_pos_1.y,
+	  				inside_pos_2.x-inside_pos_1.x,inside_pos_2.y-inside_pos_1.y);
   		}
   	});
-  if(sars[current_sar_index].pos>=0) {
+  if(sars[current_sar_index].neg>=0) {
   	findAll('Sar_signal', nodes[current_node_index], 
 	  	{'logTime':{'$gte':time.start_time+current_day_index*seconds_in_day,
 	  							'$lte':time.start_time+(current_day_index+1)*seconds_in_day},
 	  	 'tempID':sars[current_sar_index].neg}, 
 	  	{'logTime':1},{'logTime':1},function(vector){
+	  		for(var i = 1; i < vector.length; i++) {
+		  		var square_width = 10*60/seconds_in_day;
+	  			var x = (vector[i]['logTime'] - time.start_time - current_day_index*seconds_in_day)/seconds_in_day;
+					sar_context.fillStyle = "rgba(0,0,189,0.5)";
+	  			var inside_pos_1 = position_inside(x-square_width/2,1);
+	  			var inside_pos_2 = position_inside(x+square_width/2,0);
+	  			sar_context.fillRect(inside_pos_1.x,inside_pos_1.y,
+	  				inside_pos_2.x-inside_pos_1.x,inside_pos_2.y-inside_pos_1.y);
+	  		}
 	  	});
   }
   var line = 'var fields = {\''+sars[current_sar_index].id1+'.'+sars[current_sar_index].id2+'\':1};';
@@ -51,26 +76,33 @@ function draw_sar(){
   			if(max == 0) {max = 10;}
 				sar_context.fillStyle = "rgba(247,189,64,0.4)";
 				sar_context.strokeStyle="red";
-    		sar_context.lineWidth="1";
+    		sar_context.lineWidth="0.5";
+				var grd=sar_context.createRadialGradient(5,5,1,7,7,3);
+				grd.addColorStop(0,"red");
+				grd.addColorStop(1,"rgba(0,0,0,0)");
   			for(var i = 0; i < vector.length; i++) {
   				var target = vector[i];
 
   				var cur_time = target['_id'];
-  				var x = (cur_time - time.start_time - current_day_index*seconds_in_day)/seconds_in_day*canvas_size.width;
+  				var x = (cur_time - time.start_time - current_day_index*seconds_in_day)/seconds_in_day;
   				var y = 0;
 	  			if(target[sars[current_sar_index].id1]!=undefined && 
 	  				target[sars[current_sar_index].id1][sars[current_sar_index].id2]!=undefined) {
 	  				var value = target[sars[current_sar_index].id1][sars[current_sar_index].id2];
-	  				var y = value/max*(canvas_size.height-1);
-  					
+	  				y = value/max;
 	  			}
+	  			var inside_pos = position_inside(x,y);
+				sar_context.strokeStyle="red";
 	  			if(i==0){
-    				sar_context.moveTo(x,canvas_size.height-1-y);
+    				sar_context.moveTo(inside_pos.x,inside_pos.y);
     			} else {
-    				sar_context.lineTo(x,canvas_size.height-1-y);
+    				sar_context.lineTo(inside_pos.x,inside_pos.y);
     			}
-    			sar_context.stroke();
+    			//sar_context.fillStyle=grd;
+					sar_context.fillRect(inside_pos.x-2,inside_pos.y-2,4,4);
   			}
+				sar_context.strokeStyle="red";
+    		sar_context.stroke();
   	});
 }
 function draw_log(){
@@ -98,17 +130,33 @@ function reinit_node_day_log_sar(){
 	draw_sar();
 	draw_log();
 }
+function load_nodes_abstraction(){
+
+}
 $(document).ready(function(){
+	sar_context.webkitImageSmoothingEnabled = true;
 	$("#time_display").click(function(){
+		$("#node_select").slideUp();
+		$("#log_select").slideUp();
+		$("#sar_select").slideUp();
 		$("#time_select").slideToggle();
 	});
 	$("#node_display").click(function(){
+		$("#log_select").slideUp();
+		$("#sar_select").slideUp();
+		$("#time_select").slideUp();
 		$("#node_select").slideToggle();
 	});
 	$("#log_display").click(function(){
+		$("#node_select").slideUp();
+		$("#sar_select").slideUp();
+		$("#time_select").slideUp();
 		$("#log_select").slideToggle();
 	});
 	$("#sar_display").click(function(){
+		$("#node_select").slideUp();
+		$("#log_select").slideUp();
+		$("#time_select").slideUp();
 		$("#sar_select").slideToggle();
 	});
 	load_nodes();
@@ -118,25 +166,44 @@ $(document).ready(function(){
 	reinit_node_day_log_sar();
 ////////////////////////////////
 function load_nodes(){
-	var nodes_in_selector = "";
+	var str_in_selector = "<tr>";
+	var cell_in_line = 20;
+	var line_index = 0;
 	for(var i = 0; i < nodes.length; i++) {
-		nodes_in_selector += "<li index="+i+">"+nodes[i]+"</li>";
+		if(line_index == cell_in_line) {
+			line_index = 0;
+			str_in_selector += "</tr><tr>";
+		}
+		str_in_selector += "<td index="+i+">"+nodes[i]+"</td>";
+		line_index ++;
 	}
-	$("#node_select ul").append(nodes_in_selector);
-	$("#node_select li").click(function(){
+	str_in_selector += '</tr>';
+	$("#node_select table").append(str_in_selector);
+	$("#node_select td").click(function(){
 		current_node_index = $(this).attr('index');
 		reinit_node_day_log_sar();
 	});
 }
 function load_days(){
-	var days_in_selector = "";
+	var str_in_selector = "<tr>";
+	var cell_in_line = 20;
+	var line_index = 0;
+	var last_month = -1;
 	for(var i = 0; i<time.days;i++) {
 		var date = timestamp_to_monthday(time.start_time+i*seconds_in_day);
-		days_in_selector += "<li index="+i+">"+date.string
-		+"</li>";
+		console.warn(last_month);
+		if((line_index == cell_in_line)||(last_month != -1 && last_month != date.month)) {
+			line_index = 0;
+			str_in_selector += "</tr><tr>";
+			//if(last_month != date.month)
+		}
+		last_month = date.month;
+		str_in_selector += "<td index="+i+">"+date.string+"</td>";
+		line_index ++;
 	}
-	$("#time_select ul").append(days_in_selector);
-	$("#time_select li").click(function(){
+	str_in_selector += '</tr>';
+	$("#time_select table").append(str_in_selector);
+	$("#time_select td").click(function(){
 		current_day_index = $(this).attr('index');
 		reinit_node_day_log_sar();
 	});
@@ -231,5 +298,6 @@ function findAll(db, collection, criteria, fields, sort, for_all_function) {
 }
 function timestamp_to_monthday(timestamp){
 	var date = new Date((timestamp)*1000);
-	return {month:date.getMonth()+1,day:date.getDate(),string:(date.getMonth()+1)+'/'+date.getDate()};
+	var month_name = ['Jan','Feb','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	return {month:date.getMonth()+1,day:date.getDate(),string:month_name[(date.getMonth()+1)]+' '+date.getDate()};
 }
