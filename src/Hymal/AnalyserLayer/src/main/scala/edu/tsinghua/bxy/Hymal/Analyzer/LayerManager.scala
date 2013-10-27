1ece7ce9756b5d1b41e6840e5158ae30c0e93308
bxy09 <binbinbin915@live.cn>
2013-10-27 23:38:03 +0800

    add new log type

--- src/Hymal/AnalyserLayer/src/main/scala/edu/tsinghua/bxy/Hymal/Analyzer/LayerManager.scala
+++ src/Hymal/AnalyserLayer/src/main/scala/edu/tsinghua/bxy/Hymal/Analyzer/LayerManager.scala
@@ -5,9 +5,17 @@ object LayerManager {
 	def main(args:Array[String]) = {
 		val sarModule = new NodeAbstractionModule(SarAbstractionProcessor)
 		val sbatchdModule = new NodeAbstractionModule(new LogAbstractionProcessor("sbatchd"))
+		val limModule = new NodeAbstractionModule(new LogAbstractionProcessor("lim"))
+		val messagesModule = new NodeAbstractionModule(new LogAbstractionProcessor("messages"))
+		val pimModule = new NodeAbstractionModule(new LogAbstractionProcessor("pim"))
+		val resModule = new NodeAbstractionModule(new LogAbstractionProcessor("res"))
     val jobModule = new SystemAbstractionModule(JobAbstractionProcessor)
 		sarModule.run()
 		sbatchdModule.run()
+		limModule.run()
+		pimModule.run()
+		resModule.run()
+		messagesModule.run()
     	jobModule.run()
 	}
 }
